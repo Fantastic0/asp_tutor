17c1acf66c98685f37b5159e620e2939e3ab294b
Fantastico <erbolat170399@gmail.com>
2019-09-28 10:42:01 +0600

    Routing

---
+++ WebApplication1/Controllers/CalculatorController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class CalculatorController : Controller
+    {
+        public CalculatorController()
+        {
+            //ViewData["action"] = RouteData.Values["action"].ToString();
+        }
+
+        [Route("{firstNumber:int}/{secondNumber:int}")]
+        public IActionResult Sum(int firstNumber, int secondNumber)
+        {
+            ViewData["mark"] = '+';
+            ViewData["firstNumber"] = firstNumber;
+            ViewData["secondNumber"] = secondNumber;
+            ViewData["result"] = firstNumber + secondNumber;
+
+            return View("Result");
+        }
+
+        [Route("{firstNumber:int}/{secondNumber:int:min(1)}")]
+        public IActionResult Divide(int firstNumber, int secondNumber)
+        {
+            ViewData["mark"] = '/';
+            ViewData["firstNumber"] = firstNumber;
+            ViewData["secondNumber"] = secondNumber;
+            ViewData["result"] = firstNumber / secondNumber;
+
+            return View("Result");
+        }
+
+
+    }
+}
---
+++ WebApplication1/Controllers/MessagesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("Say")]
+    public class MessagesController : Controller
+    {
+        public IActionResult ShowMessage(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                ViewData["Message"] = "Message is empty";
+            }
+            else
+            {
+                ViewData["Message"] = message;
+            }
+
+            return View();
+        }
+    }
+}
